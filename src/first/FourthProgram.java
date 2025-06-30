d6a17bcf7510a876a638236eb2313617fdc0f0a7
Lokesh.R <Lokeshram2267@gmail.com>
2025-06-30 19:36:24 +0530

    Commit for the Fourth File

---
+++ src/first/FourthProgram.java
@@ -0,0 +1,13 @@
+package first;
+
+public class FourthProgram {
+
+	public static void main(String[] args) 
+	{
+		String words = "One Two Three Four";
+		int countWords = words.split("\\s").length;
+		System.out.println(countWords);
+
+	}
+
+}
