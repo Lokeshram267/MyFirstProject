b47255d3a5370d2370d61985a5c94f3decad3890
Lokesh.R <Lokeshram2267@gmail.com>
2025-07-24 17:43:57 +0530

    Commit for the Hotel class

---
+++ src/creation/Hotel.java
@@ -0,0 +1,13 @@
+package creation;
+
+public class Hotel 
+{
+   String tiffin;
+   String lunch;
+   String supper;
+   
+   void displaymenu()
+   {
+	   System.out.println(tiffin+" "+lunch+" "+supper);
+   }
+}
